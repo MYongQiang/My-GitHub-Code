1d12ca965f19ba836ec90b00c46b3c0aa7743c2d
Myongqiang <ueamyq@163.com>
2022-03-31 17:16:48 +0800

    add-code

--- C/Test_Code/Hello_world.c
+++ C/Test_Code/Hello_world.c
@@ -6,7 +6,8 @@ int main(void)
 {
     unsigned char State = 0x06;
     printf("%x\n",State &= 1 << 0);
-    while(!(State &= 1 << 0)){};
+//    while(!(State &= 1 << 0)){};
     printf("Hello world!\n");
+    
     exit(0);
 }
