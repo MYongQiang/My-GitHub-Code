ca194f4e787678843e4d2479fbc402101eef9b7e
Myongqiang <ueamyq@163.com>
2022-04-12 21:12:30 +0800

    add-code

---
+++ C/Test_Code/Prime_Number.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+//1000
+int main(void)
+{
+    int i = 0, j = 0;
+    int Num = 0;
+
+    for(i = 2; i < 1000; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            if(i % j == 0)
+            {
+                Num += j;
+ //               printf("i = %d j = %d Num = %d\n", i,j,Num);
+            }
+        }
+        if(Num == (1+i))
+        {
+             printf("%d ",i);
+        }
+        Num = 0;
+    }
+
+    exit(0);
+}
