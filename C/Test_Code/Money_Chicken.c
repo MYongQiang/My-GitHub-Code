07f7931e57edd89afeaf68a5478e267a40fcef5d
Myongqiang <ueamyq@163.com>
2022-04-12 20:11:15 +0800

    add-code

---
+++ C/Test_Code/Money_Chicken.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+//百钱买百鸡
+//公鸡:5
+//母鸡:3
+//3小鸡:1
+#define MONEY   (100)
+//100 / 5 == 20;    取商符号"/" 取余符号"%"
+//100 / 3 == 33;
+int main(void)
+{
+    int Cock = 0, Hen = 0, Chick = 0;
+
+    for(Cock = 0; Cock < 20; Cock++)
+    {
+        for(Hen = 0; Hen < 33; Hen++)
+        {
+            Chick = 100-(Cock+Hen);
+            if(Chick*(1.0/3) + Cock*5 + Hen*3 == 100)
+            {
+               printf("Cock = %d Hen = %d Chick = %d\n",Cock,Hen,Chick);
+            }
+        }
+    }
+
+    exit(0);
+}
