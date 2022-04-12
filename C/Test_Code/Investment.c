05fd4940a3b795fa32ffefc93e85203ec71cbc91
Myongqiang <ueamyq@163.com>
2022-04-12 16:27:52 +0800

    add-code

---
+++ C/Test_Code/Investment.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+
+#define INTEREST_RATE_A (0.1)   //A的年利率
+#define INTEREST_RATE_B (0.05)  //B的年利率
+
+//A以10%的单利息进行投资100美元，B以5%的复合利息机型投资100美元。计算多少年后B超过A的总投资额
+int main(void)
+{
+
+    exit(0);
+}
