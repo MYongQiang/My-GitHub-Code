fbe11da4760eda2a34f80fc3b1113dbc3e4065a4
Myongqiang <ueamyq@163.com>
2022-04-13 19:59:58 +0800

    add-code

---
+++ C/Test_Code/Scanf_1.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+//从终端输入数字直到输入q为止计算所输入数字的和
+int main(void)
+{
+    int Num = 0;
+    int Sum = 0;
+
+    while(scanf("%d",&Num) == 1)
+    {
+        Sum+= Num;
+    }
+    printf("Sum = %d\n",Sum);
+
+    exit(0);
+}
