4cfdbc964508a67dce54f559797c5e19aa42c6ee
Myongqiang <ueamyq@163.com>
2022-04-29 20:32:46 +0800

    add-code

--- C/Test_Code/Douarr_Max.c
+++ C/Test_Code/Douarr_Max.c
@@ -10,6 +10,8 @@ int main(void)
     int i = 0, j = 0, Max = 0, Line = 0,List = 0;
     int arr[M][N] = {0};
 
+    int *p = &arr[i][j];
+
 #if 1  
     for(i = 0; i < M; i++)
     {
@@ -20,6 +22,13 @@ int main(void)
     }
 #endif
 
+    for(i = 0; i < 6; i++)
+    {
+        printf("%d ",*(p++));
+    }
+    printf("\n");
+
+#if 0
     for(i = 0; i < M; i++)
     {
         for(j = 0; j < N; j++)
@@ -44,6 +53,8 @@ int main(void)
     }
 
     printf("Max = arr[%d][%d] - > %d\n",i,j,Max);
+
+#endif
 
     exit(0);
 }
