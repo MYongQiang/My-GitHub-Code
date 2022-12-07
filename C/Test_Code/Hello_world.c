b4b90f5809744f7ae37c925f462fcf7beb43cc6e
MYongQiang <usemyq@163.com>
2022-12-07 18:58:22 +0800

    测试

--- C/Test_Code/Hello_world.c
+++ C/Test_Code/Hello_world.c
@@ -7,7 +7,7 @@ int main(void)
     unsigned char State = 0x06;
     printf("%x\n",State &= 1 << 0);
 //    while(!(State &= 1 << 0)){};
-    printf("Hello world!!\n ");
+    printf("Hello world!!！\n ");
     exit(0);
 }
 
